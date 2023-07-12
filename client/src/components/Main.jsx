b22f19d30226950fabb5827ac3789dacfb1b8ca7
Rudransh <22dec007@lnmiit.ac.inm>
2023-07-12 21:57:56 +0530

    made some components

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <h1>Welcome to Deon Labs</h1>
+  )
+}
+
+export default Main
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <h1>Deon Labs</h1>
+        <p>Overview</p>
+        <p>Docs</p>
+        <p>Social</p>
+    </div>
+  )
+}
+
+export default NavBar
